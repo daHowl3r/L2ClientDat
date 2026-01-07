ddae8efb6b7f94743304de6134068628304619ab
Dr. Howl3r <spunky994@gmail.com>
2026-01-07 19:56:11 +0200

    Add JavaFX dependency

--- java/org/l2jmobius/forms/JPopupTextArea.java
+++ java/org/l2jmobius/forms/JPopupTextArea.java
@@ -16,30 +16,18 @@
  */
 package org.l2jmobius.forms;
 
-import java.awt.Frame;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Optional;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.ActionMap;
-import javax.swing.InputMap;
-import javax.swing.JComponent;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPopupMenu;
-import javax.swing.JSeparator;
-import javax.swing.JTextArea;
-import javax.swing.KeyStroke;
-import javax.swing.undo.UndoManager;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
-public class JPopupTextArea extends JTextArea
+public class JPopupTextArea extends TextArea
 {
 	private static final String COPY = "Copy (Ctrl + C)";
 	private static final String CUT = "Cut (Ctrl + X)";
@@ -49,317 +37,150 @@ public class JPopupTextArea extends JTextArea
 	private static final String GOTO = "Go to (Ctrl + G)";
 	private static final String SEARCH = "Search (Ctrl + F)";
 	
-	private final List<Integer> _lineLength;
-	protected final UndoManager _manager;
-	
 	public JPopupTextArea()
 	{
-		_lineLength = new ArrayList<>();
-		addPopupMenu();
-		_manager = new UndoManager();
-		getDocument().addUndoableEditListener(_manager);
-		
-		final Action undo = new AbstractAction()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				if (_manager.canUndo())
-				{
-					_manager.undo();
-				}
-			}
-		};
-		
-		final Action redo = new AbstractAction()
-		{
-			@Override
-			public void actionPerformed(ActionEvent e)
-			{
-				if (_manager.canRedo())
-				{
-					_manager.redo();
-				}
-			}
-		};
-		
-		final InputMap imap = getInputMap();
-		imap.put(KeyStroke.getKeyStroke("ctrl Z"), "undo");
-		imap.put(KeyStroke.getKeyStroke("ctrl Y"), "redo");
-		final ActionMap amap = getActionMap();
-		amap.put("undo", undo);
-		amap.put("redo", redo);
+		setContextMenu(createContextMenu());
+		addEventFilter(KeyEvent.KEY_PRESSED, this::handleShortcut);
 	}
 	
 	public void discardAllEdits()
 	{
-		_manager.discardAllEdits();
 	}
 	
 	public void cleanUp()
 	{
-		setText("");
-		removeAll();
+		clear();
 		discardAllEdits();
 	}
 	
-	private void addPopupMenu()
+	private ContextMenu createContextMenu()
 	{
-		final JPopupMenu menu = new JPopupMenu();
-		final JMenuItem copyItem = new JMenuItem();
-		copyItem.setAction(getActionMap().get("copy-to-clipboard"));
-		copyItem.setText(COPY);
+		final ContextMenu menu = new ContextMenu();
+		final MenuItem copyItem = new MenuItem(COPY);
+		copyItem.setOnAction(event -> copy());
+		final MenuItem cutItem = new MenuItem(CUT);
+		cutItem.setOnAction(event -> cut());
+		final MenuItem pasteItem = new MenuItem(PASTE);
+		pasteItem.setOnAction(event -> paste());
+		final MenuItem deleteItem = new MenuItem(DELETE);
+		deleteItem.setOnAction(event -> replaceSelection(""));
+		final MenuItem selectAllItem = new MenuItem(SELECT_ALL);
+		selectAllItem.setOnAction(event -> selectAll());
+		final MenuItem selectLine = new MenuItem(GOTO);
+		selectLine.setOnAction(event -> goToLine());
+		final MenuItem selectFind = new MenuItem(SEARCH);
+		selectFind.setOnAction(event -> searchString());
 		
-		final JMenuItem cutItem = new JMenuItem();
-		cutItem.setAction(getActionMap().get("cut-to-clipboard"));
-		cutItem.setText(CUT);
-		
-		final JMenuItem pasteItem = new JMenuItem();
-		pasteItem.setAction(getActionMap().get("paste-from-clipboard"));
-		pasteItem.setText(PASTE);
-		
-		final JMenuItem deleteItem = new JMenuItem();
-		deleteItem.setAction(getActionMap().get("delete-previous"));
-		deleteItem.setText(DELETE);
-		
-		final JMenuItem selectAllItem = new JMenuItem();
-		selectAllItem.setAction(getActionMap().get("select-all"));
-		selectAllItem.setText(SELECT_ALL);
-		
-		final JMenuItem selectLine = new JMenuItem();
-		selectLine.addActionListener(e -> goToLine());
-		selectLine.setText(GOTO);
-		
-		final JMenuItem selectFind = new JMenuItem();
-		selectFind.addActionListener(e -> searchString());
-		selectFind.setText(SEARCH);
-		
-		menu.add(copyItem);
-		menu.add(cutItem);
-		menu.add(pasteItem);
-		menu.add(deleteItem);
-		menu.add(new JSeparator());
-		menu.add(selectAllItem);
-		menu.add(selectLine);
-		menu.add(selectFind);
-		add(menu);
-		
-		addMouseListener(new PopupTriggerMouseListener(menu, this));
-		addKeyListener(new KeyListen());
+		menu.getItems().addAll(copyItem, cutItem, pasteItem, deleteItem, new SeparatorMenuItem(), selectAllItem, selectLine, selectFind);
+		return menu;
 	}
 	
 	protected void goToLine()
 	{
-		int no = 0;
-		boolean fnd;
-		String lineno;
-		do
+		final TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle(GOTO);
+		dialog.setHeaderText(null);
+		dialog.setContentText("Line number:");
+		final Optional<String> result = dialog.showAndWait();
+		if (result.isEmpty())
 		{
-			fnd = true;
-			lineno = JOptionPane.showInputDialog("Line number:");
-			try
-			{
-				no = Integer.parseInt(lineno);
-			}
-			catch (Exception exp)
-			{
-				if (lineno != null)
-				{
-					JOptionPane.showMessageDialog(new Frame(), "Enter a valid line number", "Error", 0);
-					fnd = false;
-				}
-			}
-			
-			if (no <= 0)
-			{
-				if (lineno == null)
-				{
-					continue;
-				}
-				
-				JOptionPane.showMessageDialog(new Frame(), "Enter a valid line number", "Error", 0);
-				fnd = false;
-			}
+			return;
 		}
 		
-		while (!fnd);
-		if (lineno != null)
+		int lineNumber = 0;
+		try
 		{
-			getLinePosition();
-			if ((no - 1) >= _lineLength.size())
-			{
-				JOptionPane.showMessageDialog(new Frame(), "Line number does not exist", "Error", 0);
-			}
-			else
-			{
-				try
-				{
-					requestFocus();
-					setCaretPosition(_lineLength.get(no - 1));
-				}
-				catch (Exception exp)
-				{
-					JOptionPane.showMessageDialog(new Frame(), "Bad position", "Error", 0);
-				}
-			}
+			lineNumber = Integer.parseInt(result.get());
 		}
+		catch (NumberFormatException ex)
+		{
+			showError("Enter a valid line number");
+			return;
+		}
+		
+		if (lineNumber <= 0)
+		{
+			showError("Enter a valid line number");
+			return;
+		}
+		
+		final int lineStart = findLineStart(getText(), lineNumber);
+		if (lineStart < 0)
+		{
+			showError("Line number does not exist");
+			return;
+		}
+		
+		requestFocus();
+		positionCaret(lineStart);
 	}
 	
 	protected void searchString()
 	{
-		final String lineno = JOptionPane.showInputDialog("Search string: ");
-		if ((lineno == null) || lineno.isEmpty())
+		final TextInputDialog dialog = new TextInputDialog();
+		dialog.setTitle(SEARCH);
+		dialog.setHeaderText(null);
+		dialog.setContentText("Search string:");
+		final Optional<String> result = dialog.showAndWait();
+		if (result.isEmpty() || result.get().isEmpty())
 		{
-			JOptionPane.showMessageDialog(new Frame(), "Enter a empty string", "Error", 0);
+			showError("Enter a search string");
 			return;
 		}
 		
-		try
+		requestFocus();
+		final String editorText = getText();
+		final String search = result.get();
+		final int start = editorText.indexOf(search, getSelection().getEnd());
+		if (start != -1)
 		{
-			requestFocus();
-			final String editorText = getText();
-			final int start = editorText.indexOf(lineno, getSelectionEnd());
-			if (start != -1)
-			{
-				setCaretPosition(start);
-				moveCaretPosition(start + lineno.length());
-				getCaret().setSelectionVisible(true);
-			}
-		}
-		catch (Exception exp)
-		{
-			JOptionPane.showMessageDialog(new Frame(), "Bad position", "Error", 0);
+			selectRange(start, start + search.length());
 		}
 	}
 	
-	private void getLinePosition()
+	private void handleShortcut(KeyEvent event)
 	{
-		_lineLength.clear();
-		final String txt = getText();
-		final int width = getWidth();
-		final StringTokenizer st = new StringTokenizer(txt, "\n ", true);
-		String str = " ";
-		int len = 0;
-		_lineLength.add(0);
-		while (st.hasMoreTokens())
+		if (!event.isControlDown())
 		{
-			final String token = st.nextToken();
-			final int w = getGraphics().getFontMetrics(getGraphics().getFont()).stringWidth(str + token);
-			if ((w > width) || (token.charAt(0) == '\n'))
-			{
-				len += str.length();
-				if (token.charAt(0) == '\n')
-				{
-					_lineLength.add(len);
-				}
-				else
-				{
-					_lineLength.add(len - 1);
-				}
-				str = token;
-			}
-			else
-			{
-				str += token;
-			}
+			return;
+		}
+		
+		if (event.getCode() == KeyCode.G)
+		{
+			goToLine();
+			event.consume();
+		}
+		else if (event.getCode() == KeyCode.F)
+		{
+			searchString();
+			event.consume();
 		}
 	}
 	
-	private static class PopupTriggerMouseListener extends MouseAdapter
+	private int findLineStart(String text, int lineNumber)
 	{
-		private final JPopupMenu popup;
-		private final JComponent component;
-		
-		public PopupTriggerMouseListener(JPopupMenu popup, JComponent component)
+		int line = 1;
+		int index = 0;
+		while (line < lineNumber)
 		{
-			this.popup = popup;
-			this.component = component;
-		}
-		
-		private void showMenuIfPopupTrigger(MouseEvent e)
-		{
-			if (e.isPopupTrigger())
+			final int next = text.indexOf('\n', index);
+			if (next < 0)
 			{
-				popup.show(component, e.getX() + 3, e.getY() + 3);
-			}
-		}
-		
-		@Override
-		public void mousePressed(MouseEvent e)
-		{
-			showMenuIfPopupTrigger(e);
-		}
-		
-		@Override
-		public void mouseReleased(MouseEvent e)
-		{
-			showMenuIfPopupTrigger(e);
-		}
-	}
-	
-	private class KeyListen implements KeyListener
-	{
-		private boolean controlDown;
-		private boolean gDown;
-		private boolean fDown;
-		
-		public KeyListen()
-		{
-		}
-		
-		@Override
-		public void keyTyped(KeyEvent e)
-		{
-		}
-		
-		@Override
-		public void keyPressed(KeyEvent e)
-		{
-			if (e.getKeyCode() == 17)
-			{
-				controlDown = true;
-			}
-			else if (e.getKeyCode() == 71)
-			{
-				gDown = true;
-			}
-			else if (e.getKeyCode() == 70)
-			{
-				fDown = true;
+				return -1;
 			}
 			
-			if (controlDown)
-			{
-				if (gDown)
-				{
-					controlDown = false;
-					gDown = false;
-					goToLine();
-				}
-				else if (fDown)
-				{
-					controlDown = false;
-					fDown = false;
-					searchString();
-				}
-			}
+			index = next + 1;
+			line++;
 		}
-		
-		@Override
-		public void keyReleased(KeyEvent e)
-		{
-			if (e.getKeyCode() == 17)
-			{
-				controlDown = false;
-			}
-			else if (e.getKeyCode() == 71)
-			{
-				gDown = false;
-			}
-			else if (e.getKeyCode() == 70)
-			{
-				fDown = false;
-			}
-		}
+		return index <= text.length() ? index : -1;
+	}
+	
+	private void showError(String message)
+	{
+		final Alert alert = new Alert(Alert.AlertType.ERROR);
+		alert.setTitle("Error");
+		alert.setHeaderText(null);
+		alert.setContentText(message);
+		alert.showAndWait();
 	}
 }
